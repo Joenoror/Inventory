c93a99627c01eb92cccb055b1a1f319db9e326ae
José Enrique Ortega Ortega <i12orore@uco.es>
2023-12-18 18:42:14 +0100

    18/12/23 - Autogenerate a false

--- app/src/main/java/com/example/inventory/data/Item.kt
+++ app/src/main/java/com/example/inventory/data/Item.kt
@@ -24,7 +24,7 @@ import androidx.room.PrimaryKey
  */
 @Entity(tableName = "items")
 data class Item(
-    @PrimaryKey(autoGenerate = true)
+    @PrimaryKey(autoGenerate = false)
     val id: Int = 0,
     val name: String,
     val price: Double,
